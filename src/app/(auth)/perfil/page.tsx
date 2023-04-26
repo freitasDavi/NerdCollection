9c54f7342125501389447eb1d5efcbc5b387bc5a
Davi <davi.frrs@outlook.com>
2023-04-26 20:12:38 -0300

    Usuarios

--- src/app/(auth)/perfil/page.tsx
+++ src/app/(auth)/perfil/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import { BonecoCard } from "@/components/BonecoCard";
+import { useState } from "react";
+
+import { productsData } from "@/database/Products"
 
 export default function Perfil() {
+    const [bonecos, setBonecos] = useState(productsData);
+
     return (
         <main className=" bg-gray-900 min-h-screen flex justify-center content-center pt-8">
             <div className="w-3/4">
@@ -15,7 +22,7 @@ export default function Perfil() {
                         <div>
                             <h2 className="text-5xl text-white mb-0 font-dongle">Oi, eu sou o Batman</h2>
                             <h4 className="font-sans text-gray-300 mb-2">Colecionador ha 5 anos</h4>
-                            <p className="font-sans text-white font-roboto">
+                            <p className="text-white font-roboto">
                                 Minha grande bio
                                 lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, enim. Quod tempore impedit nulla illo vero ut sed. Ipsum minima molestiae eligendi impedit dolor harum quibusdam odio tempora, qui id?
                                 lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore sequi, omnis ullam saepe officiis quaerat asperiores recusandae reiciendis velit. Et quo nam sunt laborum error dolores ab porro explicabo repudiandae.
@@ -25,21 +32,13 @@ export default function Perfil() {
                     <article className="flex flex-col gap-4">
                         <h2 className="text-white font-roboto text-2xl font-medium pl-4">Meus colecionáveis</h2>
                         <div id="catalog" className="grid lg:grid-cols-3 gap-8 md:grid-cols-2 sm:grid-cols-1">
-                            <BonecoCard
-                                colecao="Marvel Legends"
-                                nome="Homem de Ferro 2"
-                                imageUrl="https://cf.shopee.com.br/file/355a57a51d7d2b945a40680bf6d82d2f"
-                            />
-                            <BonecoCard
-                                colecao="DC Multiverse"
-                                nome="Batman do melhor desenho"
-                                imageUrl="https://cf.shopee.com.br/file/baf8660fd06c57858d6d34a3c9bf8078"
-                            />
-                            <BonecoCard
-                                colecao="Marvel Legends"
-                                nome="O Espetacular Homem Aranha"
-                                imageUrl="https://brincamundo.com.br/wp-content/uploads/2023/04/spiderman-action-figure.webp"
-                            />
+                            {bonecos.map((boneco) => (
+                                <BonecoCard
+                                    colecao={boneco.colecao ? boneco.colecao : "Sem coleção"}
+                                    nome={boneco.nome}
+                                    imageUrl={boneco.imageUrl}
+                                />
+                            ))}
                         </div>
                     </article>
                 </div>
---
+++ src/database/Products.ts
@@ -0,0 +1,40 @@
+
+type Product = {
+    nome: string;
+    colecao?: string;
+    categoria: string;
+    descricao: string;
+    avaliacao: number;
+    imageUrl: string;
+    marca: string;
+}
+
+export const productsData: Product[] = [
+    {
+        avaliacao: 4,
+        categoria: 'Bonequinho',
+        colecao: 'Avengers Assemble',
+        descricao: "Llaaalalal",
+        nome: "Homem de Ferro",
+        imageUrl: "https://cf.shopee.com.br/file/355a57a51d7d2b945a40680bf6d82d2f",
+        marca: 'Marvel'
+    },
+    {
+        avaliacao: 4,
+        categoria: 'Bonequinho',
+        colecao: 'DC Massa',
+        descricao: "Llaaalalal",
+        nome: "Batma",
+        imageUrl: "https://cf.shopee.com.br/file/baf8660fd06c57858d6d34a3c9bf8078",
+        marca: 'DC'
+    },
+    {
+        avaliacao: 4,
+        categoria: 'Bonequinho',
+        colecao: 'Avengers Assemble',
+        descricao: "Llaaalalal",
+        nome: "Homem Aranha",
+        imageUrl: "https://brincamundo.com.br/wp-content/uploads/2023/04/spiderman-action-figure.webp",
+        marca: 'Marvel'
+    }
+]
